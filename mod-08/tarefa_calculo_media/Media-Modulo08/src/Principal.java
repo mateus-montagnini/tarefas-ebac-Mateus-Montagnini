6169fe5e6b2ca95b658f8994d635d03308eec147
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-17 13:41:09 -0300

    Concluindo tarefa

--- mod-08/tarefa_calculo_media/Media-Modulo08/src/Principal.java
+++ mod-08/tarefa_calculo_media/Media-Modulo08/src/Principal.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
         CalculoMedia calculo = new CalculoMedia();
+        Scanner sc = new Scanner(System.in);
 
-        double media = calculo.calcularMedia(5, 5, 10, 8);
+        System.out.println("Digite a primeira nota:");
+        int nota1 = sc.nextInt();
+        System.out.println("Digite a seguunda nota:");
+        int nota2 = sc.nextInt();
+        System.out.println("Digite a terceira nota:");
+        int nota3 = sc.nextInt();
+        System.out.println("Digite a quarta nota:");
+        int nota4 = sc.nextInt();
+
+        double media = calculo.calcularMedia(nota1, nota2, nota3, nota4);
 
         System.out.println("A média do estudante é: " + media);
 
