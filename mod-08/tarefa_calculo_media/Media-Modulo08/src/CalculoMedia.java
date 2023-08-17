fc1488b0b7288649d1bdfa839ac6c28637222c35
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-17 12:51:40 -0300

    Finalizando tarefa modulo 8

--- mod-08/tarefa_calculo_media/Media-Modulo08/src/CalculoMedia.java
+++ mod-08/tarefa_calculo_media/Media-Modulo08/src/CalculoMedia.java
@@ -1,8 +1,7 @@
 public class CalculoMedia {
 
     public double calcularMedia(double nota1, double nota2, double nota3, double nota4) {
-        double media = (nota1 + nota2 + nota3 + nota4) / 4;
-        return media;
+        return (nota1 + nota2 + nota3 + nota4) / 4;
     }
 
 
