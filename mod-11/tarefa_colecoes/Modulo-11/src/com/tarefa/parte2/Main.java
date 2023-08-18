92a307d3c17337a1c2cc47b1a510d58ca333b390
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-18 14:09:06 -0300

    Concluindo parte 2 da tarefa

--- mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte2/Main.java
+++ mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte2/Main.java
@@ -1,7 +1,36 @@
 package com.tarefa.parte2;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Deseja adicionar um nome?");
+        String resposta = sc.nextLine();
+
+        List<String> listaM = new ArrayList<>();
+        List<String> listaF = new ArrayList<>();
+
+        while (resposta.equalsIgnoreCase("Sim")) {
+            System.out.println("Digite o nome e o sexo da pessoa (Mateus-M):");
+            String nome = sc.nextLine();
+            String[] nomeSplit = nome.split("-");
+            if(nomeSplit[1].contains("M")) {
+                listaM.add(nome);
+            } else if (nomeSplit[1].contains("F")){
+                listaF.add(nome);
+            }
+            System.out.println("Deseja adicionar outro nome?");
+            resposta = sc.nextLine();
+        }
+
+        Collections.sort(listaM);
+        Collections.sort(listaF);
+
+        System.out.println("Lista de pessoas do sexo masculino: " + listaM);
+        System.out.println("Lista de pessoas do sexo feminino: " + listaF);
+
+        sc.close();
     }
 }
