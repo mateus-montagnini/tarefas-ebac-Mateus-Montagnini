146acf957212e6cc78c440e47487dc35d802b7b1
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-18 14:08:59 -0300

    Concluindo parte 1 da tarefa

--- mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte1/Main.java
+++ mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte1/Main.java
@@ -1,7 +1,30 @@
 package com.tarefa.parte1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Deseja adicionar um nome?");
+        String resposta = sc.nextLine();
+
+        List<String> lista = new ArrayList<>();
+
+        while (resposta.equalsIgnoreCase("Sim")) {
+            System.out.println("Digite o nome da pessoa:");
+            String nome = sc.nextLine();
+            lista.add(nome);
+            System.out.println("Deseja adicionar outro nome?");
+            resposta = sc.nextLine();
+        }
+
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        sc.close();
     }
 }
