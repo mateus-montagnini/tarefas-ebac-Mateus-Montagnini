b915df1d044367be4259414835dc2513cce45324
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-18 13:18:24 -0300

    Criando pacotes e classes da tarefa

---
+++ mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte1/Main.java
@@ -0,0 +1,7 @@
+package com.tarefa.parte1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ mod-11/tarefa_colecoes/Modulo-11/src/com/tarefa/parte2/Main.java
@@ -0,0 +1,7 @@
+package com.tarefa.parte2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
