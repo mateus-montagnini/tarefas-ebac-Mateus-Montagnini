4ed346eae415d9160b99a1da021a4fd6c15ce831
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-19 12:26:51 -0300

    Criando Metodo para ordenar e separar as listas

--- mod-12/Colecoes_parte2/Modulo-12/src/OrdenacaoArray.java
+++ mod-12/Colecoes_parte2/Modulo-12/src/OrdenacaoArray.java
@@ -1,2 +1,18 @@
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoArray {
+
+    public void arrayOrdenado(String[] array, List<String> arrayM, List<String> arrayF) {
+        for(int i = 0; i < array.length; i++) {
+            String[] result = array[i].split("-");
+            if(result[1].contains("M")) {
+                arrayM.add(result[0]);
+            } else if(result[1].contains("F")) {
+                arrayF.add(result[0]);
+            }
+        }
+        Collections.sort(arrayM);
+        Collections.sort(arrayF);
+    }
 }
