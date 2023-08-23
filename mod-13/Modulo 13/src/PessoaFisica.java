e767ca677cef96df39740c2aaf2b98028b37b48f
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-23 09:36:00 -0300

    Criando classe PessoaFisica

---
+++ mod-13/Modulo 13/src/PessoaFisica.java
@@ -0,0 +1,35 @@
+public class PessoaFisica extends Pessoa{
+    private String cpf;
+    private int anoDeNascimento;
+    private double altura;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAnoDeNascimento() {
+        return anoDeNascimento;
+    }
+
+    public void setAnoDeNascimento(int anoDeNascimento) {
+        this.anoDeNascimento = anoDeNascimento;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public void identificacao() {
+        System.out.println("********** Pessoa Fisica **********");
+    }
+
+}
