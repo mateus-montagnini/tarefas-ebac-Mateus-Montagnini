56ddd2675604f4b5b03f0cf27192f71a4401a080
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-24 10:45:01 -0300

    Criando a classe FordFocus

---
+++ mod-15/Padroes-De-Projeto-1/src/FordFocus.java
@@ -0,0 +1,6 @@
+public class FordFocus extends Car {
+
+    public FordFocus(String modelo, int anoDoCarro, int potencia, String placa) {
+        super(modelo, anoDoCarro, potencia, placa);
+    }
+}
