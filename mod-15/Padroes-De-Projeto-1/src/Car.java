3beccbbf6be35d9ef0d5c74c18a998664e72bb0b
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-24 10:14:22 -0300

    Criando a classe Car

---
+++ mod-15/Padroes-De-Projeto-1/src/Car.java
@@ -0,0 +1,29 @@
+public abstract class Car {
+    private String modelo;
+    private int anoDoCarro;
+    private int potencia;
+    private String placa;
+
+    public Car(String modelo, int anoDoCarro, int potencia, String placa) {
+        this.anoDoCarro = anoDoCarro;
+        this.potencia = potencia;
+        this.modelo = modelo;
+        this.placa = placa;
+    }
+
+    public void modeloDoCarro() {
+        System.out.println("O modelo de carro " + modelo + " foi criado com sucesso");
+    }
+
+    public void ano() {
+        System.out.println("Este modelo foi fabricado em: " + anoDoCarro);
+    }
+
+    public void potenciaDoCarro() {
+        System.out.println("Este modelo tem " + potencia + " cavalos de potencia");
+    }
+
+    public void placaDoCarro() {
+        System.out.println("A placa do carro: " + placa);
+    }
+}
