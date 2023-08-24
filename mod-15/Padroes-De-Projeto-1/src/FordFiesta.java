c7eb1f5b9e75b39dbfb33b7e65e31c867e268e10
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-08-24 10:45:24 -0300

    Criando a classe FordFiesta

---
+++ mod-15/Padroes-De-Projeto-1/src/FordFiesta.java
@@ -0,0 +1,5 @@
+public class FordFiesta extends Car {
+    public FordFiesta(String modelo, int anoDoCarro, int potencia, String placa) {
+        super(modelo, anoDoCarro, potencia, placa);
+    }
+}
