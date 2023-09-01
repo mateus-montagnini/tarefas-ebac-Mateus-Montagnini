1d4ee12576d4e4775e5883d06f4b6c93627e1f5d
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 09:13:47 -0300

    Criando a classe ContratoDAO

---
+++ mod-24/Testes-Parte-2/src/br/com/mrocha/dao/ContratoDAO.java
@@ -0,0 +1,24 @@
+package br.com.mrocha.dao;
+
+public class ContratoDAO implements IContratoDAO {
+
+    @Override
+    public void salvar() {
+        throw new UnsupportedOperationException("Erro ao salvar contrato");
+    }
+
+    @Override
+    public void buscar() {
+        throw new UnsupportedOperationException("Erro ao buscar contrato");
+    }
+
+    @Override
+    public void excluir() {
+        throw new UnsupportedOperationException("Erro ao excluir contrato");
+    }
+
+    @Override
+    public void atualizar() {
+        throw new UnsupportedOperationException("Erro ao atualizar contrato");
+    }
+}
