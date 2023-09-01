d22147e25373875d5d3ec496fdbfb38504d44c05
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 09:14:09 -0300

    Criando a classe ClienteMock

---
+++ mod-24/Testes-Parte-2/src/br/com/mrocha/dao/mocks/ClienteMock.java
@@ -0,0 +1,26 @@
+package br.com.mrocha.dao.mocks;
+
+import br.com.mrocha.dao.IClienteDAO;
+
+public class ClienteMock implements IClienteDAO {
+
+    @Override
+    public void salvar() {
+
+    }
+
+    @Override
+    public void buscar() {
+
+    }
+
+    @Override
+    public void excluir() {
+
+    }
+
+    @Override
+    public void atualizar() {
+
+    }
+}
