9f71eac0b8ef813bd317d2e6017e2ae636c26722
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 16:14:16 -0300

    Criando interface IProdutoService

---
+++ mod-25/Projeto-Vendas-Modulo-25/src/main/services/IProdutoService.java
@@ -0,0 +1,8 @@
+package main.services;
+
+import main.domain.Produto;
+import main.services.generics.IGenericService;
+
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
