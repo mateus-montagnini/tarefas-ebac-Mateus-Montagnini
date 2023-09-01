051e97e5c03cd024130b2cd55345f1f955641ca6
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 15:53:40 -0300

    Finalizando o Mock

--- mod-25/Projeto-Vendas-Modulo-25/src/main/mocks/ClienteDaoMock.java
+++ mod-25/Projeto-Vendas-Modulo-25/src/main/mocks/ClienteDaoMock.java
@@ -1,28 +1,37 @@
 package main.mocks;
 
-import main.Cliente;
+import main.domain.Cliente;
 import main.dao.IClienteDAO;
+import main.exception.TipoChaveNaoEncontradaException;
+
+import java.util.Collection;
 
 public class ClienteDaoMock implements IClienteDAO {
+
     @Override
-    public Boolean salvar(Cliente cliente) {
+    public Boolean cadastrar(Cliente entity) throws TipoChaveNaoEncontradaException {
         return true;
     }
 
     @Override
-    public Cliente buscarPorCpf(Long cpf) {
+    public void excluir(Long valor) {
+    }
+
+    @Override
+    public void alterar(Cliente entity) throws TipoChaveNaoEncontradaException {
+    }
+
+    @Override
+    public Cliente consultar(Long valor) {
         Cliente cliente = new Cliente();
-        cliente.setCpf(cpf);
+        cliente.setCpf(valor);
         return cliente;
     }
 
     @Override
-    public void excluir(Long cpf) {
-
+    public Collection<Cliente> buscarTodos() {
+        return null;
     }
 
-    @Override
-    public void alterar(Cliente cliente) {
 
-    }
 }
