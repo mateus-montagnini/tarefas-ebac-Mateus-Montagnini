53a4752af6acce6c854795d6c8054fd8ec4b6676
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 15:52:08 -0300

    Criando mock de generics

---
+++ mod-25/Projeto-Vendas-Modulo-25/src/main/services/generics/IGenericService.java
@@ -0,0 +1,20 @@
+package main.services.generics;
+
+
+import main.domain.Persistente;
+import main.exception.TipoChaveNaoEncontradaException;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public interface IGenericService <T extends Persistente, E extends Serializable> {
+    public Boolean cadastrar(T entity) throws TipoChaveNaoEncontradaException;
+
+    public void excluir(E valor);
+
+    public void alterar(T entity) throws TipoChaveNaoEncontradaException;
+
+    public T consultar(E valor);
+    public Collection<T> buscarTodos();
+
+}
