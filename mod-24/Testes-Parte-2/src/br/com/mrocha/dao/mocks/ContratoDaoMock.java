85d50b8362285386f38ce2edb546d0794345dd99
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 09:13:41 -0300

    Criando a classe ContratoDaoMock

---
+++ mod-24/Testes-Parte-2/src/br/com/mrocha/dao/mocks/ContratoDaoMock.java
@@ -0,0 +1,26 @@
+package br.com.mrocha.dao.mocks;
+
+import br.com.mrocha.dao.IContratoDAO;
+
+public class ContratoDaoMock implements IContratoDAO {
+
+    @Override
+    public void salvar() {
+
+    }
+
+    @Override
+    public void buscar() {
+
+    }
+
+    @Override
+    public void excluir() {
+
+    }
+
+    @Override
+    public void atualizar() {
+
+    }
+}
