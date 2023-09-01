e58894bb34b2bd38f249a1fdd4e701b71c180d63
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 09:12:36 -0300

    Criando a classe AllTests

---
+++ mod-24/Testes-Parte-2/test/br/com/mrocha/AllTests.java
@@ -0,0 +1,13 @@
+package br.com.mrocha;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteServiceTest.class, ContratoServiceTest.class })
+public class AllTests {
+}
+
+
+//TODO
+//Fazer métodos de buscar, excluir e atualizar
