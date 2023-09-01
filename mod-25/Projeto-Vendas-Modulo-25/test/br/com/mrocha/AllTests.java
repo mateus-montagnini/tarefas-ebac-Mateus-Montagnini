a51b21305cfc6503b2d022d0c06fda7103df4b2e
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 15:54:12 -0300

    Criando a classe AllTests para realizar todos os testes

---
+++ mod-25/Projeto-Vendas-Modulo-25/test/br/com/mrocha/AllTests.java
@@ -0,0 +1,10 @@
+package br.com.mrocha;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClientServiceTest.class, ClienteDAOTest.class })
+public class AllTests {
+
+}
