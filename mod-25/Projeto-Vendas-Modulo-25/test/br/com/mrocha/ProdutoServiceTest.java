99af363f6269d0f9e985e18eeb7ec7f2daa23b1b
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 16:13:09 -0300

    Criando ProdutoServiceTest

---
+++ mod-25/Projeto-Vendas-Modulo-25/test/br/com/mrocha/ProdutoServiceTest.java
@@ -0,0 +1,59 @@
+package br.com.mrocha;
+
+import main.dao.IProdutoDAO;
+import main.domain.Produto;
+import main.exception.TipoChaveNaoEncontradaException;
+import main.mocks.ProdutoDaoMock;
+import main.services.IProdutoService;
+import main.services.ProdutoService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class ProdutoServiceTest {
+
+    private IProdutoService produtoService;
+
+    private Produto produto;
+
+    public ProdutoServiceTest() {
+        IProdutoDAO dao = new ProdutoDaoMock();
+        produtoService = new ProdutoService(dao);
+    }
+
+    @Before
+    public void init() {
+        produto = new Produto();
+        produto.setCodigo("C023");
+        produto.setDescricao("Camisa");
+        produto.setNome("Camisa");
+        produto.setValor(BigDecimal.TEN);
+    }
+
+    @Test
+    public void pesquisar() {
+        Produto produtor = this.produtoService.consultar(produto.getCodigo());
+        Assert.assertNotNull(produtor);
+    }
+
+    @Test
+    public void salvar() throws TipoChaveNaoEncontradaException {
+        Boolean retorno = produtoService.cadastrar(produto);
+        Assert.assertTrue(retorno);
+    }
+
+    @Test
+    public void excluir() {
+        produtoService.excluir(produto.getCodigo());
+    }
+
+    @Test
+    public void alterarCliente() throws TipoChaveNaoEncontradaException {
+        produto.setNome("Rodrigo Pires");
+        produtoService.alterar(produto);
+
+        Assert.assertEquals("Rodrigo Pires", produto.getNome());
+    }
+}
