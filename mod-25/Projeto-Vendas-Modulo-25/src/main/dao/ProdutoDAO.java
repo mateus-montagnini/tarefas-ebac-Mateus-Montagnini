0f7e15f01a155b2ae762ad731f8e68c08e84d5f3
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-01 16:13:36 -0300

    Criando ProdutoDAO

---
+++ mod-25/Projeto-Vendas-Modulo-25/src/main/dao/ProdutoDAO.java
@@ -0,0 +1,25 @@
+package main.dao;
+
+import main.dao.generics.GenericDAO;
+import main.domain.Produto;
+
+public class ProdutoDAO extends GenericDAO<Produto, String> implements IProdutoDAO {
+
+    public ProdutoDAO() {
+        super();
+    }
+
+    @Override
+    public Class<Produto> getTipoClasse() {
+        return Produto.class;
+    }
+
+    @Override
+    public void atualizarDados(Produto entity, Produto entityCadastrado) {
+        entityCadastrado.setCodigo(entity.getCodigo());
+        entityCadastrado.setDescricao(entity.getDescricao());
+        entityCadastrado.setNome(entity.getNome());
+        entityCadastrado.setValor(entity.getValor());
+    }
+
+}
