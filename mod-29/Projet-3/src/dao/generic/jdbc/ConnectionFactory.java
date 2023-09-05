3dd4e410653bf33576504931e90157c743eb0f00
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-05 12:52:42 -0300

    Criando a classe ConnectionFactory

---
+++ mod-29/Projet-3/src/dao/generic/jdbc/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package dao.generic.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static Connection connection;
+
+    private ConnectionFactory(Connection connection) {
+
+    }
+
+    public static Connection getConnection() {
+        if(connection == null) {
+
+            connection = initConnection();
+        }
+        return connection;
+    }
+
+    private static Connection initConnection() {
+        try {
+            return DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/vendas_online", "postgres", "admin");
+        }catch(SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
