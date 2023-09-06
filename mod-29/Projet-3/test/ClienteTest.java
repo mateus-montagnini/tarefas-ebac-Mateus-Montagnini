fa6e6b15de27e1533f52d686e711ec5a0e0ecaba
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-06 08:52:26 -0300

    Criando a classe ClienteTest

---
+++ mod-29/Projet-3/test/ClienteTest.java
@@ -0,0 +1,12 @@
+import dao.generic.jdbc.dao.IClienteDAO;
+import org.junit.Test;
+
+public class ClienteTest {
+
+    private IClienteDAO clienteDAO;
+
+    @Test
+    public void cadastrarTest() {
+
+    }
+}
