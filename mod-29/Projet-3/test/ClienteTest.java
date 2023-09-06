d589e3903df88cb1cf78136974a270fce8eba168
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-06 11:57:51 -0300

    Finalizando a classe ClienteTest

--- mod-29/Projet-3/test/ClienteTest.java
+++ mod-29/Projet-3/test/ClienteTest.java
@@ -1,12 +1,132 @@
+import dao.generic.jdbc.dao.ClienteDAO;
 import dao.generic.jdbc.dao.IClienteDAO;
+import domain.Cliente;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class ClienteTest {
 
     private IClienteDAO clienteDAO;
 
     @Test
-    public void cadastrarTest() {
+    public void cadastrarTest() throws Exception {
+        clienteDAO = new ClienteDAO();
 
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("05");
+        cliente.setNome("Mateus");
+        Integer countCad = clienteDAO.cadastrar(cliente);
+        assertEquals(1, (int) countCad);
+
+        Cliente clienteBD = clienteDAO.consultar("05");
+        assertNotNull(clienteBD);
+        assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+        assertEquals(cliente.getNome(), clienteBD.getNome());
+
+        Integer countDel = clienteDAO.excluir(clienteBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void consultarTeste() throws Exception {
+        clienteDAO = new ClienteDAO();
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("05");
+        cliente.setNome("Mateus");
+        Integer countCad = clienteDAO.cadastrar(cliente);
+        assertTrue(countCad == 1);
+
+        Cliente clienteBD = clienteDAO.consultar("05");
+        assertNotNull(clienteBD);
+        assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+        assertEquals(cliente.getNome(), clienteBD.getNome());
+
+        Integer countDel = clienteDAO.excluir(clienteBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void excluirTest() throws Exception {
+        clienteDAO = new ClienteDAO();
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("05");
+        cliente.setNome("Mateus");
+        Integer countCad = clienteDAO.cadastrar(cliente);
+        assertTrue(countCad == 1);
+
+        Cliente clienteBD = clienteDAO.consultar("05");
+        assertNotNull(clienteBD);
+        assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+        assertEquals(cliente.getNome(), clienteBD.getNome());
+
+        Integer countDel = clienteDAO.excluir(clienteBD);
+        assertTrue(countDel == 1);
+    }
+
+    @Test
+    public void buscarTodosTest() throws Exception {
+        clienteDAO = new ClienteDAO();
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("05");
+        cliente.setNome("Mateus");
+        Integer countCad = clienteDAO.cadastrar(cliente);
+        assertTrue(countCad == 1);
+
+        Cliente clientes = new Cliente();
+        clientes.setCodigo("08");
+        clientes.setNome("Teste");
+        Integer countCad2 = clienteDAO.cadastrar(clientes);
+        assertTrue(countCad2 == 1);
+
+        List<Cliente> list = clienteDAO.buscarTodos();
+        assertNotNull(list);
+
+
+        int countDel = 0;
+        for (Cliente cli : list) {
+            clienteDAO.excluir(cli);
+            countDel++;
+        }
+        assertEquals(list.size(), countDel);
+
+        list = clienteDAO.buscarTodos();
+        assertEquals(list.size(), 0);
+
+    }
+
+    @Test
+    public void atualizarTest() throws Exception {
+        clienteDAO = new ClienteDAO();
+
+        Cliente cliente = new Cliente();
+        cliente.setCodigo("05");
+        cliente.setNome("Mateus");
+        Integer countCad = clienteDAO.cadastrar(cliente);
+        assertEquals(1, (int) countCad);
+
+        Cliente clienteBD = clienteDAO.consultar("05");
+        assertNotNull(clienteBD);
+        assertEquals(cliente.getCodigo(), clienteBD.getCodigo());
+        assertEquals(cliente.getNome(), clienteBD.getNome());
+
+        clienteBD.setCodigo("09");
+        clienteBD.setNome("Teste2");
+        Integer countUpdate = clienteDAO.atualizar(clienteBD);
+        assertEquals(1, (int) countUpdate);
+
+        Cliente clienteBD1 = clienteDAO.consultar("11");
+        assertNull(clienteBD1);
+
+        List<Cliente> list = clienteDAO.buscarTodos();
+        for (Cliente cli : list) {
+            clienteDAO.excluir(cli);
+        }
     }
 }
