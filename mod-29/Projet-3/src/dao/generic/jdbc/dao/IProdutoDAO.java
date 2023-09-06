161444608f057eb07806c2735771ea626546884e
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-06 11:59:03 -0300

    Criando a interface IProdutoDAO

---
+++ mod-29/Projet-3/src/dao/generic/jdbc/dao/IProdutoDAO.java
@@ -0,0 +1,15 @@
+package dao.generic.jdbc.dao;
+
+import domain.Cliente;
+import domain.Produto;
+
+import java.util.List;
+
+public interface IProdutoDAO {
+
+    Integer cadastrar(Produto cliente) throws Exception;
+    Integer atualizar(Produto cliente) throws Exception;
+    Cliente consultar(String codigo) throws Exception;
+    Integer excluir(Produto clienteBD) throws Exception;
+    List<Produto> buscarTodos() throws Exception;
+}
