eb6ec5a2dcf2da42276511cc65bc44d346b0cfab
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-06 16:03:07 -0300

    Finalizando teste de cadastro

--- mod-29/Projet-3/test/ProdutoTeste.java
+++ mod-29/Projet-3/test/ProdutoTeste.java
@@ -1,2 +1,34 @@
+import dao.generic.jdbc.dao.ClienteDAO;
+import dao.generic.jdbc.dao.IProdutoDAO;
+import dao.generic.jdbc.dao.ProdutoDAO;
+import domain.Cliente;
+import domain.Produto;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class ProdutoTeste {
+
+    private IProdutoDAO produtoDAO;
+
+    @Test
+    public void cadastrarTest() throws Exception {
+        produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto();
+        produto.setCodigo("15");
+        produto.setNome("Camiseta");
+        produto.setPreco(10.50);
+        Integer countCad = produtoDAO.cadastrar(produto);
+        assertEquals(1, (int) countCad);
+
+        Produto produtoDB = produtoDAO.consultar("15");
+        assertNotNull(produtoDB);
+        assertEquals(produto.getCodigo(), produtoDB.getCodigo());
+        assertEquals(produto.getNome(), produtoDB.getNome());
+        assertEquals(produto.getPreco(), produtoDB.getPreco(), produtoDB.getPreco());
+
+//        Integer countDel = produtoDAO.excluir(produtoDB);
+//        assertEquals(1, (int) countDel);
+    }
 }
