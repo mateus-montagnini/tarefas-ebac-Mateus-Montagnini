abbb3642b1e3f7789ff1c2fb7854a6d26e3ddd43
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-06 16:36:24 -0300

    Finalizando classe AllTests

--- mod-29/Projet-3/test/AllTests.java
+++ mod-29/Projet-3/test/AllTests.java
@@ -1,2 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteTest.class, ProdutoTeste.class })
 public class AllTests {
+
 }
