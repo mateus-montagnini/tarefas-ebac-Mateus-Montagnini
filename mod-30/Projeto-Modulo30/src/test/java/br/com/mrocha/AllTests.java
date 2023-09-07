1b725451cb757514f5720eb0edf06553e65e5f3e
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-07 11:39:00 -0300

    Finalizando AllTests

--- mod-30/Projeto-Modulo30/src/test/java/br/com/mrocha/AllTests.java
+++ mod-30/Projeto-Modulo30/src/test/java/br/com/mrocha/AllTests.java
@@ -1,19 +1,12 @@
-/**
- * 
- */
-package br.com.rpires;
+package test.java.br.com.mrocha;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * @author rodrigo.pires
- *
- */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ ClienteServiceTest.class, ClienteDAOTest.class,
 	ProdutoServiceTest.class, ProdutoDAOTest.class,
-	VendaDAOTest.class})
+	})
 public class AllTests {
 
 }
