2e1959e0fb9000b357e9a755dec8259cb7c32ee1
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-07 11:44:59 -0300

    Finalizando interface IProdutoService

---
+++ mod-30/Projeto-Modulo30/src/main/java/br/com/mrocha/services/IProdutoService.java
@@ -0,0 +1,8 @@
+package main.java.br.com.mrocha.services;
+
+import main.java.br.com.mrocha.domain.Produto;
+import main.java.br.com.mrocha.services.generic.IGenericService;
+
+public interface IProdutoService extends IGenericService<Produto, String> {
+
+}
