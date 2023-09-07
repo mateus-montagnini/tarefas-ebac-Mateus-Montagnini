967334a481e236bf70455390010e95c9ed287653
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-07 11:43:50 -0300

    Finalizando interface IClienteDAO

---
+++ mod-30/Projeto-Modulo30/src/main/java/br/com/mrocha/dao/IClienteDAO.java
@@ -0,0 +1,9 @@
+package main.java.br.com.mrocha.dao;
+
+import main.java.br.com.mrocha.dao.generic.IGenericDAO;
+import main.java.br.com.mrocha.domain.Cliente;
+
+public interface IClienteDAO extends IGenericDAO<Cliente, Long> {
+
+
+}
