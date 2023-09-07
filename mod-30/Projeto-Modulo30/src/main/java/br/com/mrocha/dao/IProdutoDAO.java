8bb7638d7d6910d71dd60ea98b8c66a4e9ec3a13
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-07 11:44:50 -0300

    Finalizando interface IProdutoDAO

---
+++ mod-30/Projeto-Modulo30/src/main/java/br/com/mrocha/dao/IProdutoDAO.java
@@ -0,0 +1,8 @@
+package main.java.br.com.mrocha.dao;
+
+import main.java.br.com.mrocha.dao.generic.IGenericDAO;
+import main.java.br.com.mrocha.domain.Produto;
+
+public interface IProdutoDAO extends IGenericDAO<Produto, String> {
+
+}
