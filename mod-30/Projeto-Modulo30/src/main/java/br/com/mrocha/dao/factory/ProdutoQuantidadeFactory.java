8ae4c342b84c5ed7954e7b377a90ad31b47feb74
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-07 11:42:02 -0300

    Finalizando classe ProdutoQuantidadeFactory

--- mod-30/Projeto-Modulo30/src/main/java/br/com/mrocha/dao/factory/ProdutoQuantidadeFactory.java
+++ mod-30/Projeto-Modulo30/src/main/java/br/com/mrocha/dao/factory/ProdutoQuantidadeFactory.java
@@ -1,27 +1,19 @@
-/**
- * 
- */
-package br.com.rpires.dao.factory;
+package main.java.br.com.mrocha.dao.factory;
+import main.java.br.com.mrocha.domain.Produto;
+import main.java.br.com.mrocha.domain.ProdutoQuantidade;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import br.com.rpires.domain.Produto;
-import br.com.rpires.domain.ProdutoQuantidade;
-
-/**
- * @author rodrigo.pires
- *
- */
 public class ProdutoQuantidadeFactory {
 
 	public static ProdutoQuantidade convert(ResultSet rs) throws SQLException {
 		Produto prod = ProdutoFactory.convert(rs);
 		ProdutoQuantidade prodQ = new ProdutoQuantidade();
 		prodQ.setProduto(prod);
-		prodQ.setId(rs.getLong("ID"));
-		prodQ.setQuantidade(rs.getInt("QUANTIDADE"));
-		prodQ.setValorTotal(rs.getBigDecimal("VALOR_TOTAL"));
+		prodQ.setId(rs.getLong("id"));
+		prodQ.setQuantidade(rs.getInt("quantidade"));
+		prodQ.setValorTotal(rs.getBigDecimal("valor_total"));
 		return prodQ;
 	}
 }
