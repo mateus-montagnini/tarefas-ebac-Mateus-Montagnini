1c6f77497561d7ac0e329c881002bd57a2586145
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-08 12:16:24 -0300

    Criando classe ProdutoTeste

---
+++ mod-31/JPA-Modulo31/src/test/java/ProdutoTeste.java
@@ -0,0 +1,26 @@
+import br.com.mrocha.dao.IProdutoDAO;
+import br.com.mrocha.dao.ProdutoDAO;
+import br.com.mrocha.domain.Produto;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class ProdutoTeste {
+    private IProdutoDAO produtoDAO;
+
+    public ProdutoTeste() {
+        produtoDAO = new ProdutoDAO();
+    }
+
+    @Test
+    public void cadastrar() {
+        Produto produto = new Produto();
+        produto.setNome("Produto 1");
+        produto.setDescricao("Descricao do produto 1");
+        produto.setPreco(10.50);
+        produto = produtoDAO.cadastrar(produto);
+
+        assertNotNull(produto);
+        assertNotNull(produto.getId());
+    }
+}
