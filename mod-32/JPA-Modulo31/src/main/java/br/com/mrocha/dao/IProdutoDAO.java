07c544cfad7201f56f1513cb640b8876c0f33327
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-08 16:33:40 -0300

    Finalizando a interface IProdutoDAO

--- mod-32/JPA-Modulo31/src/main/java/br/com/mrocha/dao/IProdutoDAO.java
+++ mod-32/JPA-Modulo31/src/main/java/br/com/mrocha/dao/IProdutoDAO.java
@@ -2,7 +2,13 @@ package br.com.mrocha.dao;
 
 import br.com.mrocha.domain.Produto;
 
+import java.util.List;
+
 public interface IProdutoDAO {
 
     Produto cadastrar(Produto produto);
+    Produto consultarId(Long id);
+    void excluir(Produto produto);
+    Produto atualizar(Produto produto);
+    List<Produto> buscarTodos();
 }
