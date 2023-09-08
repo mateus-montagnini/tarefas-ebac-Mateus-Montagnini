2a9ccca7eba725de4a1e8d0e7082f7b03c147fe1
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-08 16:50:34 -0300

    Finalizando a teste de consultar produto

--- mod-32/JPA-Modulo31/src/test/java/ProdutoTeste.java
+++ mod-32/JPA-Modulo31/src/test/java/ProdutoTeste.java
@@ -1,26 +1,63 @@
 import br.com.mrocha.dao.IProdutoDAO;
 import br.com.mrocha.dao.ProdutoDAO;
 import br.com.mrocha.domain.Produto;
+import org.junit.After;
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ProdutoTeste {
     private IProdutoDAO produtoDAO;
 
-    public ProdutoTeste() {
-        produtoDAO = new ProdutoDAO();
-    }
-
-    @Test
-    public void cadastrar() {
+    private Produto criarProduto() {
         Produto produto = new Produto();
         produto.setNome("Produto 1");
         produto.setDescricao("Descricao do produto 1");
         produto.setPreco(15.50);
         produto = produtoDAO.cadastrar(produto);
-
-        assertNotNull(produto);
-        assertNotNull(produto.getId());
+        return produto;
     }
+
+    public ProdutoTeste() {
+        produtoDAO = new ProdutoDAO();
+    }
+
+    @After
+    public void end() {
+        List<Produto> lista = produtoDAO.buscarTodos();
+        lista.forEach(l -> produtoDAO.excluir(l));
+    }
+
+    @Test
+    public void cadastrarTest() {
+        Produto produto = criarProduto();
+        assertNotNull(produto);
+
+        Produto produtoDB = produtoDAO.consultarId(produto.getId());
+        assertNotNull(produtoDB);
+        assertEquals(produto.getId(), produtoDB.getId());
+        assertEquals(produto.getNome(), produtoDB.getNome());
+
+    }
+
+//    @Test
+//    public void atualizarTeste() {
+//        Produto produto = criarProduto();
+//        assertNotNull(produto);
+//
+//        Produto produtoDB = produtoDAO.consultarId(produto.getId());
+//        produtoDB.setNome("Produto Atualizado");
+//        Produto produtoAtualizado = produtoDAO.atualizar(produtoDB);
+//        assertEquals("Produto Atualizado", produtoAtualizado.getNome());
+//    }
+
+//    @Test
+//    public void consultarTeste(Long id) {
+//
+//    }
+
+    // Consultar, Excluir, Atualizar
 }
