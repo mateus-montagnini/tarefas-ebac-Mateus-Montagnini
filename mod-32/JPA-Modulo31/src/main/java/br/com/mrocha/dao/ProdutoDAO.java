a6b4fba46aea5f927c8a17730a31b6a1eebc8d07
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-08 16:33:53 -0300

    Finalizando a classe ProdutoDAO

--- mod-32/JPA-Modulo31/src/main/java/br/com/mrocha/dao/ProdutoDAO.java
+++ mod-32/JPA-Modulo31/src/main/java/br/com/mrocha/dao/ProdutoDAO.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Persistence;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 
+import java.util.List;
+
 public class ProdutoDAO implements IProdutoDAO {
 
     @Override
@@ -20,5 +22,64 @@ public class ProdutoDAO implements IProdutoDAO {
         entityManagerFactory.close();
 
         return produto;
+    }
+
+    @Override
+    public Produto consultarId(Long id) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-modulo31");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        Produto produto = entityManager.find(Produto.class, id);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return produto;
+    }
+
+    @Override
+    public void excluir(Produto produto) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-modulo31");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        produto = entityManager.merge(produto);
+        entityManager.remove(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+    }
+
+    @Override
+    public Produto atualizar(Produto produto) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-modulo31");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        produto = entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return produto;
+    }
+
+    @Override
+    public List<Produto> buscarTodos() {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-modulo31");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+        List<Produto> lista = entityManager.createQuery("SELECT p FROM Produto p", Produto.class).getResultList();
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+        entityManagerFactory.close();
+
+        return lista;
     }
 }
