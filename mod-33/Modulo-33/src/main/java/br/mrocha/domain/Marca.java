1e05b1589dffad175cc7da347d046e5c67ac87b2
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-11 09:39:10 -0300

    Criando classe Marca

--- mod-33/Modulo-33/src/main/java/br/mrocha/domain/Marca.java
+++ mod-33/Modulo-33/src/main/java/br/mrocha/domain/Marca.java
@@ -1,4 +1,17 @@
 package br.mrocha.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_marca")
 public class Marca {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "marca_seq")
+    @SequenceGenerator(name = "marca_seq", sequenceName = "sq_marca", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    @Column(name = "marca", length = 30, nullable = false)
+    private String marca;
+
 }
