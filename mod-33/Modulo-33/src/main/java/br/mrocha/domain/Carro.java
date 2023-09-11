41da358baabfce4cad094e840927a0da2442accd
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-11 09:39:16 -0300

    Criando classe Carro

--- mod-33/Modulo-33/src/main/java/br/mrocha/domain/Carro.java
+++ mod-33/Modulo-33/src/main/java/br/mrocha/domain/Carro.java
@@ -1,4 +1,56 @@
 package br.mrocha.domain;
 
+import jakarta.persistence.*;
+
+import javax.swing.*;
+
+@Entity
+@Table(name = "tb_carro")
 public class Carro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "carro_seq")
+    @SequenceGenerator(name = "carro_seq", sequenceName = "sq_carro", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    @Column(name = "Modelo", length = 30, nullable = false)
+    private String modelo;
+
+    @Column(name = "Placa", length = 10, nullable = false, unique = true)
+    private String placa;
+
+    @Column(name = "ano_fabricacao", nullable = false)
+    private Integer ano;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
 }
