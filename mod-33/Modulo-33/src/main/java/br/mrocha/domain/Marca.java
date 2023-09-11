0c883e40bcb8d2fc1629d475ca4bf254247e6723
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-11 12:09:20 -0300

    Finalizando classe Marca

--- mod-33/Modulo-33/src/main/java/br/mrocha/domain/Marca.java
+++ mod-33/Modulo-33/src/main/java/br/mrocha/domain/Marca.java
@@ -2,6 +2,8 @@ package br.mrocha.domain;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "tb_marca")
 public class Marca {
@@ -11,7 +13,32 @@ public class Marca {
     @SequenceGenerator(name = "marca_seq", sequenceName = "sq_marca", initialValue = 1, allocationSize = 1)
     private Long id;
 
-    @Column(name = "marca", length = 30, nullable = false)
-    private String marca;
+    @Column(name = "codigo", length = 10, nullable = false, unique = true)
+    private String codigo;
 
+    @Column(name = "nome", length = 30, nullable = false)
+    private String nome;
+
+    @OneToMany
+    private List<Carro> carros;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String marca) {
+        this.nome = marca;
+    }
 }
