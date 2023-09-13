ff2d0edf53ff93080366bda397218d98678f7769
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-13 09:14:42 -0300

    Cria a interface IGenericJpaService

---
+++ mod-35/Projeto-4-Modulo-35/src/main/java/br/com/mrocha/services/generic/jpa/IGenericJpaService.java
@@ -0,0 +1,23 @@
+package br.com.mrocha.services.generic.jpa;
+
+import br.com.mrocha.dao.Persistente;
+import br.com.mrocha.exceptions.DAOException;
+import br.com.mrocha.exceptions.MaisDeUmRegistroException;
+import br.com.mrocha.exceptions.TableException;
+import br.com.mrocha.exceptions.TipoChaveNaoEncontradaException;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+public interface IGenericJpaService <T extends Persistente, E extends Serializable> {
+
+    T cadastrar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+
+    void excluir(T entity) throws DAOException;
+
+    T alterar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+
+    T consultar(E valor) throws MaisDeUmRegistroException, TableException, DAOException;
+
+    Collection<T> buscarTodos() throws DAOException;
+}
