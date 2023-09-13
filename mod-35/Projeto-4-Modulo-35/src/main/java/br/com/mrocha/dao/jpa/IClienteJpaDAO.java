d44b97cddbd3684fd54429d8981454f2fcf5f71c
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-13 10:37:36 -0300

    Cria a interface IClienteJpaDAO

---
+++ mod-35/Projeto-4-Modulo-35/src/main/java/br/com/mrocha/dao/jpa/IClienteJpaDAO.java
@@ -0,0 +1,8 @@
+package br.com.mrocha.dao.jpa;
+
+import br.com.mrocha.dao.generic.jpa.IGenericJpaDAO;
+import br.com.mrocha.domain.jpa.ClienteJpa;
+
+public interface IClienteJpaDAO extends IGenericJpaDAO<ClienteJpa, Long>{
+
+}
