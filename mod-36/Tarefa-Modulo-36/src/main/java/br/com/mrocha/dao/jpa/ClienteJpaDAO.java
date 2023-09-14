1a15cf70e5d7c71bc7b867237b1c0a1c21845144
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-14 11:04:49 -0300

    Criando a classe ClienteJpaDAO

--- mod-36/Tarefa-Modulo-36/src/main/java/br/com/mrocha/dao/jpa/ClienteJpaDAO.java
+++ mod-36/Tarefa-Modulo-36/src/main/java/br/com/mrocha/dao/jpa/ClienteJpaDAO.java
@@ -3,7 +3,7 @@ package br.com.mrocha.dao.jpa;
 import br.com.mrocha.dao.generic.jpa.GenericJpaDAO;
 import br.com.mrocha.domain.jpa.ClienteJpa;
 
-public class ClienteJpaDAO extends GenericJpaDAO<ClienteJpa, Long> implements IClienteJpaDAO {
+public class ClienteJpaDAO extends GenericJpaDAO<ClienteJpa, Long> implements IClienteJpaDAO<ClienteJpa> {
 
     public ClienteJpaDAO() {
         super(ClienteJpa.class);
