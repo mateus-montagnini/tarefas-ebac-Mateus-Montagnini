65443839def04e61a75a356fc9592afdb118a94f
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-18 11:32:15 -0300

    Finalizando GenericDAO

--- mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/dao/generic/GenericDAO.java
+++ mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/dao/generic/GenericDAO.java
@@ -1,5 +1,66 @@
 package br.com.mrocha.dao.generic;
 
-public class GenericDAO {
+import java.io.Serializable;
+import java.util.Collection;
+
+import br.com.mrocha.domain.Persistente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+
+public class GenericDAO <T extends Persistente, E extends Serializable> implements IGenericDAO <T,E> {
+	
+	protected Class<T> persistenteClass;
+	
+	@PersistenceContext
+    protected EntityManager entityManager;
+	
+	public GenericDAO(Class<T> persistenteClass) {
+		this.persistenteClass = persistenteClass;
+	}
+	
+	@Override
+	public T cadastrar(T entity) {
+		entityManager.persist(entity);
+		return entity;
+	}
+
+	@Override
+	public void excluir(T entity) {
+		if (entityManager.contains(entity)) {
+            entityManager.remove(entity);
+        } else {
+            T managedCustomer = entityManager.find(this.persistenteClass, entity.getId());
+            if (managedCustomer != null) {
+                entityManager.remove(managedCustomer);
+            }
+        }
+		
+	}
+
+	@Override
+	public T alterar(T entity) {
+		entity = entityManager.merge(entity);
+		return entity;
+	}
+
+	@Override
+	public T consultar(E valor) {
+        return entityManager.find(this.persistenteClass, valor);
+	}
+
+	@Override
+	public Collection<T> buscarTodos() {
+        return entityManager.createQuery(getSql(), this.persistenteClass).getResultList();
+	}
+	
+	private String getSql() {
+        return "SELECT obj FROM " +
+				this.persistenteClass.getSimpleName() +
+				" obj";
+	}
+
 
 }
--- mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/dao/generic/IGenericDAO.java
+++ mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/dao/generic/IGenericDAO.java
@@ -1,5 +1,24 @@
 package br.com.mrocha.dao.generic;
 
-public interface IGenericDAO {
+import java.io.Serializable;
+import java.util.Collection;
 
+import br.com.mrocha.domain.Persistente;
+import br.com.mrocha.exceptions.DAOException;
+import br.com.mrocha.exceptions.MaisDeUmRegistroException;
+import br.com.mrocha.exceptions.TableException;
+import br.com.mrocha.exceptions.TipoChaveNaoEncontradaException;
+
+public interface IGenericDAO<T extends Persistente, E extends Serializable> {
+
+	T cadastrar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+	
+	T consultar(E id) throws MaisDeUmRegistroException, TableException, DAOException;
+	
+	T alterar(T entity) throws TipoChaveNaoEncontradaException, DAOException;
+	
+	void excluir(T entity) throws DAOException;
+
+	Collection<T> buscarTodos() throws DAOException;
+	
 }
