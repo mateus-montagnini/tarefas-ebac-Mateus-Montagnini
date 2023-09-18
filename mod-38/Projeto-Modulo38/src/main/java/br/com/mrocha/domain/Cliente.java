3c1ffddbbdbbb204218d2103b55c3309cbd22aaa
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-18 11:33:00 -0300

    Finalizando a pasta de domain

--- mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/domain/Cliente.java
+++ mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/domain/Cliente.java
@@ -1,5 +1,119 @@
 package br.com.mrocha.domain;
 
-public class Cliente {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "tb_cliente")
+@NamedQuery(name = "Cliente.findByNome", query = "SELECT * FROM Cliente c WHERE c.nome LIKE :nome")
+public class Cliente implements Persistente {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cliente_sequence")
+	@SequenceGenerator(name="cliente_sequence", sequenceName="sq_cliente", initialValue = 1, allocationSize = 1)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false, length = 50)
+	private String nome;
+	
+	@Column(name = "cpf", nullable = false, unique = true)
+    private Long cpf;
+    
+	@Column(name = "telefone", nullable = false)
+    private Long tel;
+	
+	@Column(name = "email", nullable = false, length = 50)
+    private String email;
+    
+	@Column(name = "endereco", nullable = false, length = 100)
+    private String end;
+    
+	@Column(name = "numero", nullable = false)
+    private Integer numero;
+    
+	@Column(name = "cidade", nullable = false, length = 100)
+    private String cidade;
+    
+	@Column(name = "estado", nullable = false, length = 50)
+    private String estado;
+    
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Long getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	
+	public Long getTel() {
+		return tel;
+	}
+	
+	public void setTel(Long tel) {
+		this.tel = tel;
+	}
+	
+	public String getEnd() {
+		return end;
+	}
+	
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	
+	public Integer getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	
+	public String getCidade() {
+		return cidade;
+	}
+	
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
+
--- mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/domain/Produto.java
+++ mod-38/Projeto-Modulo38/src/main/java/br/com/mrocha/domain/Produto.java
@@ -1,5 +1,78 @@
 package br.com.mrocha.domain;
 
-public class Produto {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produto")
+@NamedQuery(name = "Produto.findByNome", query = "SELECT c FROM Produto c WHERE c.nome LIKE :nome") 
+public class Produto implements Persistente {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="prod_sequence")
+	@SequenceGenerator(name="prod_sequence", sequenceName="sq_produto", initialValue = 1, allocationSize = 1)
+	private Long id;
+
+	@Column(name = "codigo", nullable = false, length = 10, unique = true)
+	private String codigo;
+	
+	@Column(name = "nome", nullable = false, length = 50)
+	private String nome;
+	
+	@Column(name = "descricao", nullable = false, length = 50)
+	private String descricao;
+	
+	@Column(name = "valor", nullable = false)
+	private BigDecimal valor;
+	
+	
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 }
