4af903094d7e5d52e64b648188bd725595d41ffc
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-21 09:43:49 -0300

    Finalizando a classe de CadastroCliente

--- mod-42/ClienteServer/src/main/java/br/com/mrocha/vendas/online/service/CadastroCliente.java
+++ mod-42/ClienteServer/src/main/java/br/com/mrocha/vendas/online/service/CadastroCliente.java
@@ -1,4 +1,30 @@
 package br.com.mrocha.vendas.online.service;
 
+import br.com.mrocha.vendas.online.domain.Cliente;
+import br.com.mrocha.vendas.online.repository.IClienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CadastroCliente {
+
+    private IClienteRepository clienteRepository;
+
+    @Autowired
+    public CadastroCliente(IClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente create(@Valid Cliente cliente) {
+        return this.clienteRepository.insert(cliente);
+    }
+
+    public Cliente update(@Valid Cliente cliente) {
+        return this.clienteRepository.save(cliente);
+    }
+
+    public void delete(String id) {
+        this.clienteRepository.deleteById(id);
+    }
 }
