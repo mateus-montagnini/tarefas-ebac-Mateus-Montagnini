f7c425bb2836790ad57e5ce738e55cefe52a0c7e
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-21 06:27:40 -0300

    Criando o repository Cliente

---
+++ mod-42/ClienteServer/src/main/java/br/com/mrocha/vendas/online/repository/IClienteRepository.java
@@ -0,0 +1,13 @@
+package br.com.mrocha.vendas.online.repository;
+
+import br.com.mrocha.vendas.online.domain.Cliente;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IClienteRepository extends MongoRepository<Cliente, String> {
+
+    Optional<Cliente> findByCpf(Long cpf);
+}
