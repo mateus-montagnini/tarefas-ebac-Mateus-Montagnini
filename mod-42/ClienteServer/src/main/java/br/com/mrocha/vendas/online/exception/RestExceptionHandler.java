11bf6058a4d93c8f2c5e746100c7c61b40931053
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-21 09:34:48 -0300

    Finalizando a classe de RestExceptionHandler

---
+++ mod-42/ClienteServer/src/main/java/br/com/mrocha/vendas/online/exception/RestExceptionHandler.java
@@ -0,0 +1,132 @@
+package br.com.mrocha.vendas.online.exception;
+
+import br.com.mrocha.vendas.online.error.APIError;
+import jakarta.persistence.EntityNotFoundException;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(
+            @NotNull MissingServletRequestParameterException ex, HttpHeaders headers,
+            HttpStatus status, WebRequest request) {
+        String error = ex.getParameterName() + " parameter is missing";
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage(error);
+        return buildResponseEntity(apiError);
+    }
+
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(
+            @NotNull HttpMediaTypeNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        ex.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+        APIError apiError = new APIError(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+        apiError.setMessage(builder.substring(0, builder.length() - 2));
+        return buildResponseEntity(apiError);
+    }
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            @NotNull MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage("Validation error");
+        apiError.addValidationErrors(ex.getBindingResult().getFieldErrors());
+        apiError.addValidationError(ex.getBindingResult().getGlobalErrors());
+        return buildResponseEntity(apiError);
+    }
+
+    protected ResponseEntity<Object> handleBindException(@NotNull BindException ex, HttpHeaders headers, HttpStatus status,
+                                                         WebRequest request) {
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+//    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
+//    protected ResponseEntity<Object> handleConstraintViolation(
+//            javax.validation.@NotNull ConstraintViolationException ex) {
+//        APIError apiError = new APIError(BAD_REQUEST);
+//        apiError.setMessage("Validation error");
+//        apiError.addValidationErrors(ex.getConstraintViolations());
+//        return buildResponseEntity(apiError);
+//    }
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(@NotNull EntityNotFoundException ex) {
+        APIError apiError = new APIError(NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    protected ResponseEntity<Object> handleBadRequest(@NotNull BadRequestException ex) {
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(UnsupportedOperationException.class)
+    protected ResponseEntity<Object> handleUnsupportedOperationException(@NotNull UnsupportedOperationException ex) {
+        APIError apiError = new APIError(HttpStatus.UNPROCESSABLE_ENTITY);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    protected ResponseEntity<Object> handleDuplicateKeyException(@NotNull DuplicateKeyException ex) {
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(NotFound.class)
+    protected ResponseEntity<Object> handleNotFound(@NotNull NotFound ex) {
+        APIError apiError = new APIError(NOT_FOUND);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(BadRequest.class)
+    protected ResponseEntity<Object> handleBadRequest(@NotNull BadRequest ex) {
+        APIError apiError = new APIError(BAD_REQUEST);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(HttpClientErrorException.UnprocessableEntity.class)
+    protected ResponseEntity<Object> handleUnprocessableEntity(HttpClientErrorException.@NotNull UnprocessableEntity ex) {
+        APIError apiError = new APIError(HttpStatus.UNPROCESSABLE_ENTITY);
+        apiError.setMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    @Contract("_ -> new")
+    private @NotNull ResponseEntity<Object> buildResponseEntity(APIError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+
+}
