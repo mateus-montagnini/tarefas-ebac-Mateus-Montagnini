cc6da67d217d55b540d7bb21d0476eeed8526a39
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-22 09:58:04 -0300

    Finalizando a classe de BadRequestException

---
+++ mod-42/ProdutoServer/src/main/java/br/com/mrocha/vendas/online/exception/BadRequestException.java
@@ -0,0 +1,10 @@
+package br.com.mrocha.vendas.online.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    private static final long serialVersionUID = -7339546357706827674L;
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
