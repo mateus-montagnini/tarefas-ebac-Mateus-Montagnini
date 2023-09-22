43f177e84698e80661d899b4177b945e6e49ed07
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-22 09:59:45 -0300

    Finalizando a classe de ProdutoServerApplication

--- mod-42/ProdutoServer/src/main/java/br/com/mrocha/vendas/online/ProdutoServerApplication.java
+++ mod-42/ProdutoServer/src/main/java/br/com/mrocha/vendas/online/ProdutoServerApplication.java
@@ -1,9 +1,14 @@
 package br.com.mrocha.vendas.online;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ProdutoServerApplication {
 
 	public static void main(String[] args) {
