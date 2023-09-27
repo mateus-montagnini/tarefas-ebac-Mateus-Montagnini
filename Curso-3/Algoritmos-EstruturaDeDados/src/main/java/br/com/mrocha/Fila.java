bf40c053683af10a8fc1b2a9db81a5ca8cfe57d2
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-27 10:14:57 -0300

    Criando a classe de Fila

---
+++ Curso-3/Algoritmos-EstruturaDeDados/src/main/java/br/com/mrocha/Fila.java
@@ -0,0 +1,37 @@
+package br.com.mrocha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fila {
+
+    private List<Integer> lista;
+
+    public Fila() {
+        this.lista = new ArrayList<>();
+    }
+
+    public void enqueue() {
+
+    }
+
+    public void dequeue() {
+
+    }
+
+    public Integer rear() {
+        return null;
+    }
+
+    public Integer front() {
+        return null;
+    }
+
+    public Integer size() {
+        return null;
+    }
+
+    public Boolean isEmpty() {
+        return true;
+    }
+}
