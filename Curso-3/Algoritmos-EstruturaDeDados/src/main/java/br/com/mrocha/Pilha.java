b0cba02a0fa5b2a2a10f116f008628cfc19ac56d
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-09-27 10:10:13 -0300

    Criando a classe de Pilha

---
+++ Curso-3/Algoritmos-EstruturaDeDados/src/main/java/br/com/mrocha/Pilha.java
@@ -0,0 +1,32 @@
+package br.com.mrocha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pilha {
+    private List<Integer> lista;
+
+    public Pilha() {
+        this.lista = new ArrayList<>();
+    }
+
+    public void push() {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public Integer top() {
+        return null;
+    }
+
+    public Boolean isEmpty(){
+        return true;
+    }
+
+    public Integer size() {
+        return null;
+    }
+}
