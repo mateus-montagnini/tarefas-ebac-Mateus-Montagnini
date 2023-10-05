d563c57db8207350534d65d84d5860f44b7c09a7
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-05 10:50:46 -0300

    Finalizando o algoritmo recursivo para calcular o fatorial

--- Curso-3/Recursividade-ProgramacaoDinamica/src/main/java/br/com/mrocha/CalculoFatorialRecursivo.java
+++ Curso-3/Recursividade-ProgramacaoDinamica/src/main/java/br/com/mrocha/CalculoFatorialRecursivo.java
@@ -1,4 +1,18 @@
 package br.com.mrocha;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculoFatorialRecursivo {
+
+    public static long calcFatorial(int n) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+       return n * calcFatorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calcFatorial(20));
+    }
 }
