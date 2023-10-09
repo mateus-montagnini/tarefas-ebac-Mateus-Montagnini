8ea6df4ba5a84e2516ccfdcdb0936c8b67506808
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-09 08:21:21 -0300

    Finalizando a tarefa do modulo 2

--- Curso-3/Recursividade-ProgramacaoDinamica/src/main/java/br/com/mrocha/CalculoFatorialRecursivo.java
+++ Curso-3/Recursividade-ProgramacaoDinamica/src/main/java/br/com/mrocha/CalculoFatorialRecursivo.java
@@ -3,14 +3,14 @@ package br.com.mrocha;
 public class CalculoFatorialRecursivo {
 
     public static long calcFatorial(int n) {
-        if(n == 0 || n == 1) {
+        if(n <= 1) {
             return 1;
         }
        return n * calcFatorial(n - 1);
     }
 
     public static void main(String[] args) {
-        int n = 100;
+        int n = 50;
         System.out.println("Elemento " + n + ": " + calcFatorial(n));
     }
 }
