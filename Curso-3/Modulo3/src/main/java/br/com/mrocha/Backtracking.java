68ac99b28eb82d319ce4be325574694b6d31c238
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-10-11 06:53:15 -0300

    Metodo para gerar subsets

--- Curso-3/Modulo3/src/main/java/br/com/mrocha/Backtracking.java
+++ Curso-3/Modulo3/src/main/java/br/com/mrocha/Backtracking.java
@@ -5,8 +5,21 @@ import java.util.List;
 
 public class Backtracking {
     public static List<List<Integer>> findSubsets(int[] S, int n) {
-        List<List<Integer>> result = new ArrayList<>();
-        generateSubsets(S, n, 0, new ArrayList<>(), result);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        generateSubsets(S, n, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private static void generateSubsets(int[] S, int n, int inicio, List<Integer> currentSubset, List<List<Integer>> res) {
+        if (n == 0) {
+            res.add(new ArrayList<>(currentSubset));
+            return;
+        }
+
+        for (int i = inicio; i < S.length; i++) {
+            currentSubset.add(S[i]);
+            generateSubsets(S, n - 1, i + 1, currentSubset, res);
+            currentSubset.remove(currentSubset.size() - 1);
+        }
     }
 }
